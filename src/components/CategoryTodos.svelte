25011fefb3fb0eaadbdaf2bd3f607d05d4605ddf
Leni Kadali Mutungi <leni1@users.noreply.github.com>
2021-09-26 12:01:15 +0300

    Rename meta description

--- src/components/CategoryTodos.svelte
+++ src/components/CategoryTodos.svelte
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="All product categories available on our site">
+    <meta name="description" content="All Todos available in this category">
     <!-- TODO: Dynamically fill -->
     <title>Todos in this Category</title>
 </head>
